c27745a809c30ea9c13b1858ff26e380a90e7832
Rongrrr <wrr3232@163.com>
2023-06-07 20:14:54 +0800

    数据文件上传

---
+++ data/goods_db.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: select * from project.goods_db
+LIMIT 0, 1000
+
+-- Date: 2023-06-07 20:12
+*/
+INSERT INTO `` (`id`,`name`,`goods_num`,`goods_price`,`type`,`sale`,`remark`) VALUES (2,'生椰拿铁',10,'18','饮料','10',NULL);
+INSERT INTO `` (`id`,`name`,`goods_num`,`goods_price`,`type`,`sale`,`remark`) VALUES (5,'臻选咖啡豆',100,'40','咖啡豆','7',NULL);
+INSERT INTO `` (`id`,`name`,`goods_num`,`goods_price`,`type`,`sale`,`remark`) VALUES (6,'可可蛋糕',100,'9','糕点','20',NULL);
+INSERT INTO `` (`id`,`name`,`goods_num`,`goods_price`,`type`,`sale`,`remark`) VALUES (7,'卡布奇诺',80,'13','饮料','0',NULL);
+INSERT INTO `` (`id`,`name`,`goods_num`,`goods_price`,`type`,`sale`,`remark`) VALUES (8,'进口咖啡豆',100,'80','咖啡豆','0',NULL);
+INSERT INTO `` (`id`,`name`,`goods_num`,`goods_price`,`type`,`sale`,`remark`) VALUES (9,'冰美式',200,'10','饮料','0',NULL);
+INSERT INTO `` (`id`,`name`,`goods_num`,`goods_price`,`type`,`sale`,`remark`) VALUES (10,'牛角可颂',20,'8','糕点','10',NULL);
---
+++ data/user_db.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: select * from project.user_db
+LIMIT 0, 1000
+
+-- Date: 2023-06-07 20:13
+*/
+INSERT INTO `` (`id`,`username`,`password`,`email`,`avatar`,`date`,`identity`,`status`) VALUES (2,'lihua','123456','lihua@test.com',NULL,'2023-03-18 14:23:05.000000','employee',0);
+INSERT INTO `` (`id`,`username`,`password`,`email`,`avatar`,`date`,`identity`,`status`) VALUES (4,'limin','$2b$10$05Hh549pgnvGiHz96mQDWunoJcERXlZx4Pu1fY4FAQw6xpgsBdaIO','limin@test.com',NULL,'2023-03-18 14:26:25.000000','employee',0);
+INSERT INTO `` (`id`,`username`,`password`,`email`,`avatar`,`date`,`identity`,`status`) VALUES (6,'xiaomin','$2b$10$YKnhHVriuBrSbjFY5pK86uJ0hShqYyWe4hDIF76Hsc6ZWYAoopK0G','xiaomin@min.com','//www.gravatar.com/avatar/4dba091b30c4908703299f14510d890a?s=200&r=pg&d=cc','2023-03-18 14:45:52.000000','employee',0);
+INSERT INTO `` (`id`,`username`,`password`,`email`,`avatar`,`date`,`identity`,`status`) VALUES (7,'lily999','$2b$10$7gbF2guIXGK2NsrwRvZrHuDaKM/ZQjuJh0ZlMRewFE6k3MrcsyyBe','lily@qq.com','//www.gravatar.com/avatar/1325437ab2c5e0836fcd452fb3cec6bc?s=200&r=pg&d=cc','2023-03-18 17:36:07.000000','employee',0);
+INSERT INTO `` (`id`,`username`,`password`,`email`,`avatar`,`date`,`identity`,`status`) VALUES (8,'test','$2b$10$9VMUITYBBXQnQq/MzXL6peCgpX2M9W2wdWOqVHdWsYcXkzk.pi/Oe','test@qq.com','//www.gravatar.com/avatar/bf58432148b643a8b4c41c3901b81d1b?s=200&r=pg&d=cc','2023-03-29 15:50:17.000000','employee',0);
+INSERT INTO `` (`id`,`username`,`password`,`email`,`avatar`,`date`,`identity`,`status`) VALUES (9,'admin','$2b$10$ythEdy.OwBKJekqOiyzU6uBAm1vbeUs8PpzRb6gYojYnR2ZFQrdaG','admin@admin.com','//www.gravatar.com/avatar/64e1b8d34f425d19e1ee2ea7236d3028?s=200&r=pg&d=cc','2023-03-30 13:20:17.000000','manager',0);
+INSERT INTO `` (`id`,`username`,`password`,`email`,`avatar`,`date`,`identity`,`status`) VALUES (10,'wang','$2b$10$M00nzXpqoquqaadVz3S0XOnkkTnEwR0clsWQvr4fQ0dp6oP.Q4evC','wang123@163.com','//www.gravatar.com/avatar/c933d2e27f7f476fe5822e86b2f1f48a?s=200&r=pg&d=cc','2023-04-13 17:40:10.000000','employee',0);
